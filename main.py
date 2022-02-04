4eea82e9a749fca262fa08e9335c5f517aa553d6
fdeh <gdeh75@gmail.com>
2022-02-04 14:59:34 +0300

    refacror

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://auto.ria.com/newauto/marka-mazda/'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 HOST = 'https://auto.ria.com'
+
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
@@ -28,12 +30,12 @@ def get_content(html):
     print(cars)
 
 
-
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
         get_content(html.text)
     else:
         print('Error')
+
 
 parse()
