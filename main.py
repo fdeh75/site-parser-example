eeba52bf3242efa577fd632f5a4d188592c66fb6
fdeh <gdeh75@gmail.com>
2022-02-04 14:58:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ria.com/newauto/marka-mazda/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+HOST = 'https://auto.ria.com'
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('section', class_='proposition  ')
+
+    cars = []
+    for item in items:
+        cars.append({
+            'title': item.find('div', class_='na-gallery-view  list-view').get_text(tsrip=True),
+            'title': item.find('a', class_='proposition_link').get(href),
+            'title': item.find('div', class_='proposition_area').get_text(strip=True),
+            'title': item.find('h3', class_='proposition_name').get_text(strip=True),
+            'title': item.find('span', class_='link').get(section),
+            'link': HOST + item.find('a', class_='proposition_link').get(href),
+        })
+    print(cars)
+
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Error')
+
+parse()
